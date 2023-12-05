6ac5c5af877a23a2f0f96f4bf8f7abceaf3fe7f0
Baskh <Basilelkhodr@gmail.com>
2023-12-05 16:04:40 +0200

    Alot of changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'gallerypainters';
+  navigateToSignUp() {
+    this.router.navigate(['/signup']); // Replace '/sign-up' with the appropriate route for <app-sign-up>]
+  }
+  navigateToSignIn() {
+    this.router.navigate(['/signin']);
+  }
 }
